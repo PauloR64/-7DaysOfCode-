2556988175e54600ba025fe3b8eecf83d735dd8d
Paulo Roberto <134407510+PauloR64@users.noreply.github.com>
2023-11-06 09:35:25 -0300

    Create Day2.js

---
+++ Day2.js
@@ -0,0 +1,8 @@
+let nome = 'Paulo';
+let idade = '23';
+let linguagem = 'JavaScript'
+
+
+const resposta = `Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!`;
+
+console.log(resposta);
